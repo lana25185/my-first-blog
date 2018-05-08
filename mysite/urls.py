6b5450d45025d0fb9a9c9a1fff5db9babb51ef02
Lana <lana_p85@ukr.net>
2018-05-09 00:53:11 +0300

    mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
+
+from django.contrib.auth import views as auth_views
+url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete')
